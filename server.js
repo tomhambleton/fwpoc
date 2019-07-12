de342b04a195e2dfcdf37b285f27e1915bd0fe97
Thomas Hambleton <tomhambleton@gmail.com>
2019-07-12 16:05:21 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: './public'})
+})
+
+app.get('/flipper', function(req, res) {
+  res.sendFile('video.html', {root: './public'})
+})
+
+
+app.get('/video', function(req, res) {
+  const path = 'assets/flipper.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
+app.listen(8080, function () {
+  console.log('Listening on port 8080!')
+})
